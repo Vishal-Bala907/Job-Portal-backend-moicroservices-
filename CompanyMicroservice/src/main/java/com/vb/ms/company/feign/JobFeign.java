b3898155adb4f347ca833e58a2a6ab90ba5c716a
Vishal bala <vishal.bala.100@gmail.com>
2024-11-23 21:50:22 +0530

    config server added

--- CompanyMicroservice/src/main/java/com/vb/ms/company/feign/JobFeign.java
+++ CompanyMicroservice/src/main/java/com/vb/ms/company/feign/JobFeign.java
@@ -3,7 +3,6 @@ package com.vb.ms.company.feign;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
